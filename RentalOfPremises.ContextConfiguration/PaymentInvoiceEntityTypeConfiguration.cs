ca147d6bb3d95845f6bd0fa37b53b12a5b46c266
Lendaar <voronenkov04@mail.ru>
2024-05-09 15:24:48 +0300

    Описал конфигурацию контекста

---
+++ RentalOfPremises.ContextConfiguration/PaymentInvoiceEntityTypeConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using RentalOfPremises.Context.Contracts.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace RentalOfPremises.ContextConfiguration
+{
+    public class PaymentInvoiceEntityTypeConfiguration : IEntityTypeConfiguration<PaymentInvoice>
+    {
+        public void Configure(EntityTypeBuilder<PaymentInvoice> builder)
+        {
+            builder.ToTable("PaymentInvoices");
+            builder.HasIdAsKey();
+            builder.PropertyAuditConfiguration();
+            builder.Property(x => x.NumberContract).IsRequired();
+            builder.Property(x => x.PeriodPayment).IsRequired();
+            builder.Property(x => x.Electricity).IsRequired();
+            builder.Property(x => x.WaterPl).IsRequired();
+            builder.Property(x => x.WaterMi).IsRequired();
+            builder.Property(x => x.PassPerson).IsRequired();
+            builder.Property(x => x.PassLegСar).IsRequired();
+            builder.Property(x => x.PassGrСar).IsRequired();
+            builder.HasIndex(x => x.NumberContract)
+                .HasFilter($"{nameof(PaymentInvoice.DeletedAt)} is null")
+                .HasDatabaseName($"IX_{nameof(PaymentInvoice)}_{nameof(PaymentInvoice.NumberContract)}");
+        }
+    }
+}
---
+++ RentalOfPremises.ContextConfiguration/RoomEntityTypeConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using RentalOfPremises.Context.Contracts.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace RentalOfPremises.ContextConfiguration
+{
+    public class RoomEntityTypeConfiguration : IEntityTypeConfiguration<Room>
+    {
+        public void Configure(EntityTypeBuilder<Room> builder)
+        {
+            builder.ToTable("Rooms");
+            builder.HasIdAsKey();
+            builder.PropertyAuditConfiguration();
+            builder.Property(x => x.Liter).IsRequired();
+            builder.Property(x => x.NumberRoom).IsRequired();
+            builder.Property(x => x.SquareRoom).IsRequired();
+            builder.Property(x => x.Occupied).IsRequired();
+            builder.Property(x => x.TypeRoom).IsRequired();
+            builder.HasIndex(x => x.Liter)
+                .HasFilter($"{nameof(Room.DeletedAt)} is null")
+                .HasDatabaseName($"IX_{nameof(Room)}_{nameof(Room.Liter)}");
+            builder.HasMany(x => x.Contract)
+                .WithOne(x => x.Room)
+                .HasForeignKey(x => x.RoomId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
