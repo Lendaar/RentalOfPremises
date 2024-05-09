3bf50d81967cf385ca6bd3c5d5371d93a86c2293
Lendaar <voronenkov04@mail.ru>
2024-05-09 17:37:38 +0300

    Написан контекст и сделана инициализирующая миграция

---
+++ RentalOfPremises.Context/RentalOfPremisesContext.cs
@@ -0,0 +1,71 @@
+﻿using RentalOfPremises.Common.Entity.InterfaceDB;
+using RentalOfPremises.Context.Contracts.Interface;
+using RentalOfPremises.Context.Contracts.Models;
+using RentalOfPremises.ContextConfiguration;
+using Microsoft.EntityFrameworkCore;
+
+namespace RentalOfPremises.Context
+{
+    public class RentalOfPremisesContext : DbContext,
+        IRentalOfPremisesContext,
+        IDbRead,
+        IDbWriter,
+        IUnitOfWork
+    {
+        /// <summary>
+        /// Контекст работы с БД
+        /// </summary>
+        /// <remarks>
+        /// 1) dotnet tool install --global dotnet-ef
+        /// 2) dotnet tool update --global dotnet-ef
+        /// 3) dotnet ef migrations add [name] --project TimeTable203.Context\TimeTable203.Context.csproj
+        /// 4) dotnet ef database update --project TimeTable203.Context\TimeTable203.Context.csproj
+        /// 5) dotnet ef database update [targetMigrationName] --TimeTable203.Context\TimeTable203.Context.csproj
+        /// </remarks>
+        public RentalOfPremisesContext(DbContextOptions<RentalOfPremisesContext> options) : base(options) { }
+
+        public DbSet<Contract> Contracts { get; set; }
+
+        public DbSet<PaymentInvoice> PaymentInvoices { get; set; }
+
+        public DbSet<Price> Prices { get; set; }
+
+        public DbSet<Room> Rooms { get; set; }
+
+        public DbSet<Tenant> Tenants { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(IContextConfigurationAnchor).Assembly);
+        }
+
+        IQueryable<TEntity> IDbRead.Read<TEntity>()
+            => base.Set<TEntity>()
+                .AsNoTracking()
+                .AsQueryable();
+
+        void IDbWriter.Add<TEntities>(TEntities entity)
+            => base.Entry(entity).State = EntityState.Added;
+
+        void IDbWriter.Update<TEntities>(TEntities entity)
+              => base.Entry(entity).State = EntityState.Modified;
+
+        void IDbWriter.Delete<TEntities>(TEntities entity)
+              => base.Entry(entity).State = EntityState.Deleted;
+
+
+        async Task<int> IUnitOfWork.SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            var count = await base.SaveChangesAsync(cancellationToken);
+            foreach (var entry in base.ChangeTracker.Entries().ToArray())
+            {
+                entry.State = EntityState.Detached;
+            }
+            return count;
+        }
+    }
+}
---
+++ RentalOfPremises.Context/SampleContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace RentalOfPremises.Context
+{
+    /// <summary>
+    /// Файбрика для создания контекста в DesignTime (Миграции)
+    /// </summary>
+    public class SampleContextFactory : IDesignTimeDbContextFactory<RentalOfPremisesContext>
+    {
+        public RentalOfPremisesContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            var options = new DbContextOptionsBuilder<RentalOfPremisesContext>()
+                .UseSqlServer(connectionString)
+                .Options;
+
+            return new RentalOfPremisesContext(options);
+        }
+    }
+}
