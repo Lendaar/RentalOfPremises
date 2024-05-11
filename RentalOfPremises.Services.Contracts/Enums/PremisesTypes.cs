39d003d1121498f6e58ef8b23d140c661c0da751
Lendaar <voronenkov04@mail.ru>
2024-05-11 19:55:07 +0300

    Добавил enum-типы в Services

---
+++ RentalOfPremises.Services.Contracts/Enums/PremisesTypes.cs
@@ -0,0 +1,18 @@
+﻿namespace RentalOfPremises.Services.Contracts.Enums
+{
+    /// <summary>
+    /// Типы помещений
+    /// </summary>
+    public enum PremisesTypes
+    {
+        /// <summary>
+        /// Cобственное нежилое помещение
+        /// </summary>
+        OwnNonResidentialPremises,
+
+        /// <summary>
+        /// Временное бытовое помещение
+        /// </summary>
+        TemporaryLivingPremises
+    }
+}
---
+++ RentalOfPremises.Services.Contracts/Enums/TenantTypes.cs
@@ -0,0 +1,18 @@
+﻿namespace RentalOfPremises.Services.Contracts.Enums
+{
+    /// <summary>
+    /// Роли пользователей
+    /// </summary>
+    public enum TenantTypes
+    {
+        /// <summary>
+        /// Юр лицо
+        /// </summary>
+        Legal,
+
+        /// <summary>
+        /// ИП
+        /// </summary>
+        Individual
+    }
+}
