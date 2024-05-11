c82a4277e619b2e71d879596d8efbda37c6f7869
Lendaar <voronenkov04@mail.ru>
2024-05-11 14:56:45 +0300

    Создал реализацию репозиториев чтения

---
+++ RentalOfPremises.Repositories/Implementations/ContractReadRepository.cs
@@ -0,0 +1,30 @@
+﻿using RentalOfPremises.Common.Entity.InterfaceDB;
+using RentalOfPremises.Common.Entity.Repositories;
+using RentalOfPremises.Context.Contracts.Models;
+using RentalOfPremises.Repositories.Contracts.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace RentalOfPremises.Repositories.Implementations
+{
+    public class ContractReadRepository : IContractReadRepository, IRepositoriesAnchor
+    {
+        private readonly IDbRead reader;
+
+        public ContractReadRepository(IDbRead reader)
+        {
+            this.reader = reader;
+        }
+
+        Task<IReadOnlyCollection<Contract>> IContractReadRepository.GetAllAsync(CancellationToken cancellationToken)
+            => reader.Read<Contract>()
+                .NotDeletedAt()
+                .OrderBy(x => x.DateStart)
+                .ToReadOnlyCollectionAsync(cancellationToken);
+
+        Task<Contract?> IContractReadRepository.GetByIdAsync(Guid id, CancellationToken cancellationToken)
+             => reader.Read<Contract>()
+                .NotDeletedAt()
+                .ById(id)
+                .FirstOrDefaultAsync(cancellationToken);
+    }
+}
---
+++ RentalOfPremises.Repositories/Implementations/RoomReadRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentalOfPremises.Common.Entity.InterfaceDB;
+using RentalOfPremises.Common.Entity.Repositories;
+using RentalOfPremises.Context.Contracts.Models;
+using RentalOfPremises.Repositories.Contracts.Interface;
+
+namespace RentalOfPremises.Repositories.Implementations
+{
+    public class RoomReadRepository : IRoomReadRepository, IRepositoriesAnchor
+    {
+        private readonly IDbRead reader;
+
+        public RoomReadRepository(IDbRead reader)
+        {
+            this.reader = reader;
+        }
+
+        Task<IReadOnlyCollection<Room>> IRoomReadRepository.GetAllAsync(CancellationToken cancellationToken)
+            => reader.Read<Room>()
+               .NotDeletedAt()
+               .OrderBy(x => x.Liter)
+               .ToReadOnlyCollectionAsync(cancellationToken);
+
+        Task<Room?> IRoomReadRepository.GetByIdAsync(Guid id, CancellationToken cancellationToken)
+             => reader.Read<Room>()
+               .NotDeletedAt()
+               .ById(id)
+               .FirstOrDefaultAsync(cancellationToken);
+
+        Task<Dictionary<Guid, Room>> IRoomReadRepository.GetByIdsAsync(IEnumerable<Guid> ids, CancellationToken cancellation)
+            => reader.Read<Room>()
+               .NotDeletedAt()
+               .ByIds(ids)
+               .OrderBy(x => x.Liter)
+               .ToDictionaryAsync(key => key.Id, cancellation);
+
+        Task<bool> IRoomReadRepository.AnyByIdAsync(Guid id, CancellationToken cancellationToken)
+            => reader.Read<Room>()
+               .NotDeletedAt()
+               .ById(id)
+               .AnyAsync(cancellationToken);
+    }
+}
