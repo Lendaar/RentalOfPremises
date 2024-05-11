a3ddf28a0b653342aa82c6e100f1288bbfd65787
Lendaar <voronenkov04@mail.ru>
2024-05-11 19:55:27 +0300

    Добавил модели сущностей

---
+++ RentalOfPremises.Services.Contracts/Models/RoomModel.cs
@@ -0,0 +1,45 @@
+﻿using RentalOfPremises.Services.Contracts.Enums;
+
+namespace RentalOfPremises.Services.Contracts.Models
+{
+    /// <summary>
+    /// Модель помещения
+    /// </summary>
+    public class RoomModel
+    {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Литер
+        /// </summary>
+        public string Liter { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Номер помещения
+        /// </summary>
+        public int NumberRoom { get; set; }
+
+        /// <summary>
+        /// Площадь
+        /// </summary>
+        public double SquareRoom { get; set; }
+
+        /// <summary>
+        /// Тип помещения
+        /// </summary>
+        public PremisesTypes TypeRoom { get; set; }
+
+        /// <summary>
+        /// Цена аренды
+        /// </summary>
+        public double? Price { get; set; }
+
+        /// <summary>
+        /// Занято ли помещение
+        /// </summary>
+        public bool Occupied { get; set; } = false;
+    }
+}
---
+++ RentalOfPremises.Services.Contracts/Models/UserModel.cs
@@ -0,0 +1,30 @@
+﻿using RentalOfPremises.Services.Contracts.Enums;
+
+namespace RentalOfPremises.Services.Contracts.Models
+{
+    /// <summary>
+    /// Модель пользователя
+    /// </summary>
+    public class UserModel
+    {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Логин пользователя
+        /// </summary>
+        public string LoginUser { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Пароль пользователя
+        /// </summary>
+        public string PasswordUser { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Роль пользователя
+        /// </summary>
+        public RoleTypes RoleUser { get; set; }
+    }
+}
