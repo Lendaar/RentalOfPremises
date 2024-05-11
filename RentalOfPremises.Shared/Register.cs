e6c633c70b89551b59c623ec15ae1ca6a178c8e5
Lendaar <voronenkov04@mail.ru>
2024-05-11 14:57:43 +0300

    Создал регистрацию репозиториев по маркерному интерфейсу

---
+++ RentalOfPremises.Shared/Register.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace RentalOfPremises.Shared
+{
+    public static class Register
+    {
+        public static void AssemblyInterfaceAssignableTo<TInterface>(this IServiceCollection services, ServiceLifetime lifetime)
+        {
+            var serviceType = typeof(TInterface);
+            var types = serviceType.Assembly.GetTypes()
+                .Where(x => serviceType.IsAssignableFrom(x) && !(x.IsAbstract || x.IsInterface));
+            foreach (var type in types)
+            {
+                services.TryAdd(new ServiceDescriptor(type, type, lifetime));
+                var interfaces = type.GetTypeInfo().ImplementedInterfaces
+                .Where(i => i != typeof(IDisposable) && i.IsPublic && i != serviceType);
+                foreach (var interfaceType in interfaces)
+                {
+                    services.TryAdd(new ServiceDescriptor(interfaceType,
+                        provider => provider.GetRequiredService(type),
+                        lifetime));
+                }
+            }
+        }
+    }
+}
