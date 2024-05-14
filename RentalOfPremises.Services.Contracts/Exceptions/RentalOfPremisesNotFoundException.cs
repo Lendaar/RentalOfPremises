f1db7d93d457c65ab7024f4386aec1cfdb84b6db
Lendaar <voronenkov04@mail.ru>
2024-05-14 21:11:15 +0300

    Создал исключения для валидации

---
+++ RentalOfPremises.Services.Contracts/Exceptions/RentalOfPremisesNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿namespace RentalOfPremises.Services.Contracts.Exceptions
+{
+    /// <summary>
+    /// Запрашиваемый ресурс не найден
+    /// </summary>
+    public class RentalOfPremisesNotFoundException : RentalOfPremisesException
+    {
+        /// <summary>
+        /// Инициализирует новый экземпляр <see cref="RentalOfPremisesNotFoundException"/> с указанием
+        /// сообщения об ошибке
+        /// </summary>
+        public RentalOfPremisesNotFoundException(string message)
+            : base(message)
+        { }
+    }
+}
---
+++ RentalOfPremises.Services.Contracts/Exceptions/RentalOfPremisesValidationException.cs
@@ -0,0 +1,23 @@
+﻿using RentalOfPremises.Shared;
+
+namespace RentalOfPremises.Services.Contracts.Exceptions
+{
+    /// <summary>
+    /// Ошибки валидации
+    /// </summary>
+    public class RentalOfPremisesValidationException : RentalOfPremisesException
+    {
+        /// <summary>
+        /// Ошибки
+        /// </summary>
+        public IEnumerable<InvalidateItemModel> Errors { get; }
+
+        /// <summary>
+        /// Инициализирует новый экземпляр <see cref="RentalOfPremisesValidationException"/>
+        /// </summary>
+        public RentalOfPremisesValidationException(IEnumerable<InvalidateItemModel> errors)
+        {
+            Errors = errors;
+        }
+    }
+}
